a6800c403e3fce473b50125c34ff0dc0c8b8d284
raphael.guye <raphael.guye@ec219558-cbc6-dad2-ff86-7b7d1ac634b6>
2010-04-14 08:41:53 +0000

    Merge between our basic canvas and the GUI_example.py.

--- trunk/GuyeNeuhaus.py
+++ trunk/GuyeNeuhaus.py
@@ -3,30 +3,91 @@
 """
 GuyeNeuhaus.py
 
-Created by Raphaël Guye and Jonathan Neuhaus on 2010-04-14.
+Created by Raphael Guye and Jonathan Neuhaus on 2010-04-14.
 Copyright (c) 2010 HE-Arc. All rights reserved.
 """
 
-
 import pygame
+from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
+import sys
+
+cities = []
+
+screen_x = 500
+screen_y = 500
+
+city_color = [10,10,200] # blue
+city_radius = 3
+
+font_color = [255,255,255] # white
 
 class City(object):
-	"""Representation of a city"""
-	def __init__(self, x, y, name):
-		self.x = x
-		self.y = y
-		self.name = name
-		
+    """Representation of a city"""
+    def __init__(self, x, y, name):
+        self.x = x
+        self.y = y
+        self.name = name
 
 def main():
-	pass
+    initPygame()
+    draw(cities)
 
+    collecting = True
+	
+    while collecting:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            elif event.type == KEYDOWN and event.key == K_RETURN:
+                collecting = False
+            elif event.type == MOUSEBUTTONDOWN:
+                cities.append(pygame.mouse.get_pos())
+                draw(cities)
+                print "draw() called"
+                #print cities
+    screen.fill(0)
+	
+    print cities
+    ga_solve(cities)
+    print cities
+    pygame.draw.lines(screen,city_color,True,cities)
+    text = font.render("Un chemin, pas le meilleur!", True, font_color)
+    textRect = text.get_rect()
+    screen.blit(text, textRect)
+    pygame.display.flip()
+    while True:
+        event = pygame.event.wait()
+        if event.type == KEYDOWN: break
+
+def initPygame():
+    global screen
+    global font
+    pygame.init() 
+    window = pygame.display.set_mode((screen_x, screen_y)) 
+    pygame.display.set_caption('Exemple') 
+    screen = pygame.display.get_surface() 
+    font = pygame.font.Font(None,30)
+	
+
+def draw(positions):
+    screen.fill(0)
+    for pos in positions: 
+            pygame.draw.circle(screen,city_color,pos,city_radius)
+    text = font.render("Nombre: %i" % len(positions), True, font_color)
+    textRect = text.get_rect()
+    screen.blit(text, textRect)
+    pygame.display.flip()
 
 def ga_solve(file=None, gui=True, maxtime=0):
-	""" Résolution du voyageur de commerce """
-	pass
+    """ Salesman resolution """
+    global cities
+    new = []
+    for c in cities:
+            new.insert(0,c)
+    cities = new
+    pass
 
 
 if __name__ == '__main__':
-	main()
+    main()
 

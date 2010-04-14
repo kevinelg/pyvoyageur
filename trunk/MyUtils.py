c6db6b5efb55b93adf343f508656e839c86fdcb3
neuhaus.jonathan <neuhaus.jonathan@ec219558-cbc6-dad2-ff86-7b7d1ac634b6>
2010-04-14 18:33:47 +0000

    * Optimisation of the speed Measurement

--- trunk/MyUtils.py
+++ trunk/MyUtils.py
@@ -9,17 +9,12 @@ Created by Jonathan Neuhaus on 2010-04-14.
 Copyright (c) 2010 HE-Arc. All rights reserved.
 """
 
-# import sys
-# import os
-
 def speedMeasure(func):
 	"""Time elapsed during the function"""
 	import time
 	def wrapper(*arg):
-		t1 = time.clock()
+		t1 = time.time()
 		res = func(*arg)
-		t2 = time.clock()
-		print "%s took %0.3f ms" % (func.func_name, (t2-t1)*1000.0)
+		print "%s took %0.3f ms" % (func.func_name, (time.time()-t1)*1000.0)
 		return res
 	return wrapper
-

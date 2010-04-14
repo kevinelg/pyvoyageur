ebf010a03059d7db96934116e25032c10c1ed698
neuhaus.jonathan <neuhaus.jonathan@ec219558-cbc6-dad2-ff86-7b7d1ac634b6>
2010-04-14 08:16:01 +0000

    Initial commit: * Add ressource files * Add specifications * Add initial source file

---
+++ GuyeNeuhaus.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+GuyeNeuhaus.py
+
+Created by Raphaël Guye and Jonathan Neuhaus on 2010-04-14.
+Copyright (c) 2010 HE-Arc. All rights reserved.
+"""
+
+
+import pygame
+
+class City(object):
+	"""Representation of a city"""
+	def __init__(self, x, y, name):
+		self.x = x
+		self.y = y
+		self.name = name
+		
+
+def main():
+	pass
+
+
+def ga_solve(file=None, gui=True, maxtime=0):
+	""" Résolution du voyageur de commerce """
+	pass
+
+
+if __name__ == '__main__':
+	main()
+

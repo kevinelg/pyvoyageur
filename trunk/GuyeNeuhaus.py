44d27f85d6053a5295669cbfb079bba8c414f3b3
neuhaus.jonathan <neuhaus.jonathan@ec219558-cbc6-dad2-ff86-7b7d1ac634b6>
2010-04-20 08:23:49 +0000

    * Initial generation of the routes

--- trunk/GuyeNeuhaus.py
+++ trunk/GuyeNeuhaus.py
@@ -14,6 +14,8 @@ import os
 from string import strip
 from optparse import OptionParser
 from MyUtils import speedMeasure
+import math
+from random import randint
 
 try:
 	import psyco
@@ -37,6 +39,9 @@ city_radius = 3
 font_color = [255,255,255] # white
 
 
+initialRoutesNumber = 30
+
+
 #================================================
 #              Classes
 #================================================
@@ -49,8 +54,39 @@ class City(object):
         self.name = name
         
     def __str__(self):
-        return self.name, " : ", self.x, self.y
+        return self.name + " : " + str(self.x) +","+ str(self.y)
 
+class Route(object):
+    """Route with all cities"""
+    def __init__(self, cityList):
+    	self.cityList = cityList
+    def len(self):
+        """Process the length of a route"""
+        length = 0
+        for i in range(len(self.cityList)-1):
+    		length += dist2City(self.cityList[i], self.cityList[i+1])
+        return length
+    def isEgal(self, route):
+        """Check if the 2 listRoutes are equal"""
+        if (len(self.cityList) <> route.len()):
+            return False
+        for i in range(len(self.cityList)):
+            if (self.cityList[i]<>route[i]):
+                return False
+        return True
+    def isContained(self, listRoutes):
+        """Check if the 2 listRoutes are equal"""
+        for r in listRoutes:
+            if r.isEgal(self):
+                return True
+        return False
+    def __str__(self):
+        tmp = ""
+        for c in self.cityList:
+            tmp += str(c)
+            tmp += " / "
+        return tmp
+    		
 
 #================================================
 #              Main
@@ -101,6 +137,69 @@ It is an optional argument."""
 #================================================
 #              Methodes
 #================================================
+def fac(n):
+    """Return the orial of a number"""
+    p=1
+    for i in range(1,n+1):
+        p*=i
+    return p
+
+def dist2City(city1, city2):
+	"""return the distance between 2 cities"""
+	return math.sqrt((city1.x-city2.x)**2 + (city1.y-city2.y)**2)
+	
+def generateRoutes(listRoutes, baseRoute, initialRoutesNumber):
+    """generate the number of listRoutes required"""
+    listRoutes.append(baseRoute)
+    cpt=1
+    cityList = []
+    # The city list contains already the last city
+    lenCityList = len(baseRoute.cityList) - 1
+    indexList = []
+    while cpt<=initialRoutesNumber and cpt<=maxPossibilities(lenCityList):
+        tmp = shake(lenCityList)
+        print "lenCityList", lenCityList
+        while tmp in indexList:
+            tmp = shake(lenCityList)
+        indexList.append(tmp)
+        print "il", indexList
+        print cpt
+        cpt += 1
+        
+        
+        
+        
+        
+    #     cityList[:] = []
+    #     for i in indexList:
+    #         cityList.append(baseRoute.cityList[i])
+    #     #if (Route(cityList) not in listRoutes):
+    #     
+    #     newRoute = Route(cityList)
+    #     if (not newRoute.isContained(listRoutes)):
+    #         listRoutes.append(Route(cityList))
+    #     cpt += 1
+    #     print cpt
+    # print "out"
+
+def shake(initialList):
+    """Return a list shaken"""
+    indexList = []
+    # The first element is allways the starting city
+    indexList.append(0)
+    
+    for i in range(initialList-1):
+        i = randint(1,initialList-1)
+        while (i in indexList):
+            i = randint(1,initialList-1)
+        indexList.append(i)
+    # Return to the starting city
+    indexList.append(0)
+    return indexList
+    
+def maxPossibilities(lenCityList):
+    """return the maximum number of different listRoutes"""
+    return fac(lenCityList-1)
 
 def initPygame():
     global screen
@@ -135,8 +234,20 @@ def ga_solve(file=None, gui=True, maxtime=0):
     
     new = []
     for c in cities:
-            new.insert(0,c)
+		new.insert(0,c)
+		print "x,y=",c.x,",",c.y
+    
     cities = new
+    cities.append(cities[0])
+    
+    # Repeat the first element to the last position
+    baseRoute = Route(cities)
+    
+    listRoutes=[]
+    print fac(len(baseRoute.cityList))
+    generateRoutes(listRoutes, baseRoute, initialRoutesNumber)
+    for r in listRoutes:
+        print r
     
     pos = []
     [pos.append((c.x,c.y)) for c in cities]
